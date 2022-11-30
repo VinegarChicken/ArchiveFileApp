929bd4c9b8babbabc5e7728bc40adbb62e81f7a6
Milo Auguste III <miloaugusteiii@outlook.com>
2022-11-29 21:16:03 -0500

    ui stuff start

--- main.cpp
+++ main.cpp
@@ -1,65 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include "main.h"
+#include <wx/listctrl.h>
 
-struct CppResult{
-    bool isErr;
-    void* val;
-};
 
-struct CppArray{
-    void** ptr;
-    size_t size;
-    size_t cap;
-};
-//WxWidgets dir examples: test/streams: 8836, test/benchmarks: 8861
-
-struct ZipFileArchive{
-
-};
-
-extern "C" CppResult zfa_new(const char*);
-extern "C" CppResult zfa_extract(ZipFileArchive*, size_t, char**, bool);
-extern "C" CppResult zfa_extract_all(ZipFileArchive*, char**);
-extern "C" CppArray zfa_listall(ZipFileArchive*);
-extern "C" CppArray zfa_list_files_in_dir(ZipFileArchive*, size_t);
-
-int main(int args, char** argv){
-    CppResult zfa_ = zfa_new(argv[1]);
-    if(zfa_.isErr){
-        std::cout<<(const char*) zfa_.val;
-    }
-   ZipFileArchive* zfa = (ZipFileArchive*) zfa_.val;
-    char* path = "C:\\Users\\m\\Downloads\\ryujinxggfgggdg";
-    //CppResult res = zfa_extract_all(zfa, &path);
-    //std::cout<<(char*) res.val;
-  /*
-  
-  */
-  CppArray ListDir = zfa_list_files_in_dir(zfa, 8861);
-  char** arr = (char**) ListDir.ptr;
-  for(int i=0;i<ListDir.size;i++){
-    std::cout<<arr[i]<<std::endl;
-  }
- /*
- ArrayStuct list = zfa_listall(zfa);
-  std::map<int, char*> ZfaMap;
-  std::map<int, char*>::iterator itr;
-    char** arr = (char**) list.ptr;
-    for(int i=0;i<list.size;i++){
-        ZfaMap.insert(std::pair<int, char*>(i, arr[i]));
-    }
-    for(itr=ZfaMap.begin();itr!=ZfaMap.end();itr++){
-        std::cout<<itr->first<<" "<<itr->second<<std::endl;
-    }
- */
-  
-  
-    
-    return 0;
-}
-
-/*// wxWidgets "Hello World" Program
+// wxWidgets "Hello World" Program
  
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
@@ -73,7 +19,6 @@ class MyApp : public wxApp
 public:
     virtual bool OnInit();
 };
-extern "C" void hello_world(); 
 class MyFrame : public wxFrame
 {
 public:
@@ -83,11 +28,16 @@ private:
     void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
+    void OnFileSelect(wxCommandEvent& ev);
+    void OnFileClicked(wxMouseEvent& ev);
+    bool isSelectedItem;
+
 };
  
 enum
 {
-    ID_Hello = 1
+    ID_Hello = 1,
+    ID_ARCHIVELIST = 2
 };
  
 wxIMPLEMENT_APP(MyApp);
@@ -107,7 +57,19 @@ MyFrame::MyFrame()
                      "Help string shown in status bar for this menu item");
     menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
- 
+    wxPanel* panel = new wxPanel(this);
+
+    wxListCtrl* listCtrl = new wxListCtrl(panel, ID_ARCHIVELIST, {10, 10}, {330, 200}, wxLC_REPORT|wxSIMPLE_BORDER);
+    listCtrl->AppendColumn("Name", wxLIST_FORMAT_LEFT, 80);
+     listCtrl->InsertItem(0, "First");
+     listCtrl->InsertItem(1, "Second");
+    listCtrl->Connect(ID_ARCHIVELIST, wxEVT_COMMAND_LIST_ITEM_SELECTED, wxCommandEventHandler(MyFrame::OnFileSelect), nullptr, this);
+    listCtrl->Connect(ID_ARCHIVELIST, wxEVT_COMMAND_LIST_ITEM_DESELECTED, wxCommandEventHandler(MyFrame::OnFileSelect), nullptr, this);
+    listCtrl->Connect(ID_ARCHIVELIST, wxEVT_COMMAND_LIST_ITEM_DESELECTED, wxMouseEventHandler(MyFrame::OnFileClicked), nullptr, this);
+
+      //listCtrl->SetItem(0, 2, "5359");
+     // listCtrl->SetItem(0, 3, "This is the first item");
+
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
  
@@ -129,16 +91,29 @@ void MyFrame::OnExit(wxCommandEvent& event)
 {
     Close(true);
 }
- 
+void MyFrame::OnFileSelect(wxCommandEvent& ev){
+    
+    if(this->isSelectedItem){
+        this->isSelectedItem = false;
+    }
+    else{
+        this->isSelectedItem = true;
+    }
+    
+
+    std::cout<<"[MyFrame::OnFileSelect] selected ! " + std::to_string(this->isSelectedItem)<<std::endl;
+}
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
     wxMessageBox("This is a wxWidgets Hello World example",
                  "About Hello World", wxOK | wxICON_INFORMATION);
 }
- 
+void MyFrame::OnFileClicked(wxMouseEvent& ev){
+    if(this->isSelectedItem){
+        std::cout<<"[MyFrame::OnFileClicked] Selected and Clicked !"<<std::endl;
+    }
+}
 void MyFrame::OnHello(wxCommandEvent& event)
 {
-    hello_world();
     wxLogMessage("Hello world from wxWidgets!");
 }
-*/
